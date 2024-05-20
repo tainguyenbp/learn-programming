6300aaf3cfd42dbf618a43445b0cb0b68d3d65c5
tainguyenbp <nguyenngoctaibp@gmail.com>
2024-05-20 11:41:20 +0700

    test println macro

---
+++ rust-basic/learn01/hello-world-macro.rs
@@ -0,0 +1,44 @@
+fn main() {
+
+    println!("{}, {}!", "Hello", "world"); // Hello, world!
+    println!("{}, {}!, {}", "Hello", "world", "devops"); // Hello, world!, devops
+
+    println!("{0}, {1}!", "Hello", "world"); // Hello, world!
+
+    println!("{0}, {1}!, {2}", "Hello", "world", "devops"); // Hello, world!, devops
+
+
+    println!("{greeting}, {name}!", greeting = "Hello", name = "world"); // Hello, world!
+
+    println!("{greeting}, {name}!, {job}",  greeting = "Hello", name = "world", job="devops"); // Hello, world!, devops
+
+
+    let (greeting, name) = ("hello", "world"); // 💡 Two Variable bindings declare & initialize in one line.
+    println!("{greeting}, {name}!"); // Hello, world!
+
+    let (greeting, name, job) = ("tainguyenbp", "sre", "devops"); // 💡 Two Variable bindings declare & initialize in one line.
+    println!("{greeting}, {name}!, {job}"); // tainguyenbp, sre!, devops
+
+    println!("{:?}", [1, 2, 3]); // [1, 2, 3]
+    println!("{:?}", [1, 2, 3]); // [1, 2]
+    println!("{:?}", [1]); // [1]
+    println!("{:?}", [1, 2, 3, 4]); // [1, 2, 3, 4]
+
+    println!("{:#?}", [1, 2, 3, 4]);
+
+    let x = format!("{}, {}!", "Hello", "world");
+    println!("{}", x); // Hello, world!
+
+
+    let infor = format!("{} {}, {}", "tai", "nguyen", "devops");
+    println!("{}", infor); 
+
+    // 💡 Rust has a print!() macro as well
+    print!("Hello, world!"); // Without new line
+    println!(); // A new line
+
+    print!("tai, nguyen, devops"); // Without new line
+    println!(); // A new line
+
+    print!("Hello, world!\n"); // With new line
+}
---
+++ rust-basic/learn01/readme.md
@@ -0,0 +1,9 @@
+### Compile it with main.rs
+```
+rustc hello-wold.rs
+./hello-wold
+
+rustc hello-wold-macro.rs
+./hello-wold-macro
+
+```
