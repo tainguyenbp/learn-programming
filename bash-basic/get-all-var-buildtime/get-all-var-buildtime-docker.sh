ee19f983ee5bc83e067e6f7d35197aa90a82e78a
tainguyenbp <nguyenngoctaibp@gmail.com>
2024-05-21 14:24:04 +0700

    get all variable to docker build

---
+++ bash-basic/get-all-var-buildtime/get-all-var-buildtime-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Function to parse Dockerfile and extract ARG variable names
+parse_dockerfile() {
+    arg_names=()
+    while IFS= read -r line; do
+        if [[ $line =~ ^ARG\ (.+)$ ]]; then
+            arg_names+=("${BASH_REMATCH[1]}")
+        fi
+    done < Dockerfile
+}
+
+# Function to construct the docker build command with --build-arg flags
+construct_build_command() {
+    build_command=("docker" "build")
+    added_args=()  # Array to keep track of added arguments
+    for arg_name in "${arg_names[@]}"; do
+        # Check if the argument has already been added
+        if [[ ! " ${added_args[@]} " =~ " ${arg_name} " ]]; then
+            build_command+=("--build-arg" "${arg_name}=\${$arg_name}")
+            added_args+=("${arg_name}")
+        fi
+    done
+    build_command+=(".")
+}
+
+# Main function
+main() {
+    parse_dockerfile
+    construct_build_command
+    echo "${build_command[@]}"
+    # Uncomment the below line to execute the docker build command
+    # eval "${build_command[@]}"
+}
+main
