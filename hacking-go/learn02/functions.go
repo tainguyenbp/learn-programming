654e561e2443a2fd60aee432e7b7997129c4c88c
tainguyenbp <nguyenngoctaibp@gmail.com>
2024-05-29 08:00:20 +0700

    learning function golang
    learning function golang

--- hacking-go/learn02/functions.go
+++ hacking-go/learn02/functions.go
@@ -26,6 +26,11 @@ func addTwo2(x int, y int) (xPlusTwo int, yPlusTwo int) {
 	yPlusTwo = y + 2
 	return xPlusTwo, yPlusTwo
 }
+
+func init() {
+	fmt.Println("Executing init function!")
+}
+
 func main() {
 
 	fmt.Println("multiply: ", multiply(10, 20))
@@ -33,6 +38,17 @@ func main() {
 
 	addTwo_x, addTwo_y := addTwo(5, 10)
 
+	addTwos_x, addTwos_y := addTwos(9, 8)
+
+	fmt.Println(addTwo(5, 10))
 	fmt.Println("addTwo: ", addTwo_x, addTwo_y)
+
+	fmt.Println(addTwos(9, 8))
+	fmt.Println("addTwos: ", addTwos_x, addTwos_y)
+
+	addTwo2_x, addTwo2_y := addTwo2(7, 7)
 	fmt.Println(addTwo2(20, 30))
+	fmt.Println("addTwo2: ", addTwo2_x, addTwo2_y)
+
+	fmt.Println("Executing main!")
 }
--- hacking-go/learn02/readme.md
+++ hacking-go/learn02/readme.md
@@ -1,1 +1,11 @@
 ### 02.1 - Packages, functions, variables, basic types, casting and constants
+
+
+
+```
+init function
+init function is used to set up the state. A common practice is to declare flags in it.
+1. Imported packages are initialized.
+2. Variable declarations evaluate their initializers.
+3. init function execute
+```
